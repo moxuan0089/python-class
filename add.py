3fd409415006e7dfda05582477b8899c814c33d5
moxuan0089 <105497126+moxuan0089@users.noreply.github.com>
2022-05-13 20:30:28 +0800

    add

---
+++ add.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def add(x):
+    v=0
+        
+    for i in range(1, x+1):
+        v+=i
+            
+    return v
+
+z = add(10)
+print(z)
